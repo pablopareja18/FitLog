663b923239638187733e1657f928b04865f82daa
pablopareja18 <pparejaherrero@gmail.com>
2026-04-14 11:53:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,17 @@
-// Cache buster - limpia todo y fuerza recarga
+// FitLog SW - sin caché, siempre sirve la versión más reciente
+const VERSION = 'fitlog-v3';
+
 self.addEventListener('install', () => self.skipWaiting());
+
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
       .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.matchAll({ includeUncontrolled: true }))
-      .then(clients => clients.forEach(c => c.navigate(c.url)))
+      .then(() => self.clients.claim())
   );
+});
+
+// Sin caché: siempre va a la red
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => new Response('Sin conexión', { status: 503 })));
 });
